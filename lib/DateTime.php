a8e2563c3d7b011ea0d99067784383b5f4ca0438
Renaud Kyoku <676896+rkyoku@users.noreply.github.com>
2022-02-01 23:17:17 +0100

    Update DateTime.php
    PHP 8

--- lib/DateTime.php
+++ lib/DateTime.php
@@ -84,7 +84,7 @@ class DateTime extends \DateTime implements DateTimeInterface
 	 * @param string $format A format string accepted by get_format()
 	 * @return string formatted date and time string
 	 */
-	public function format($format)
+	public function format(string $format) : string
 	{
 		return parent::format(self::get_format($format));
 	}
@@ -177,7 +177,7 @@ class DateTime extends \DateTime implements DateTimeInterface
 		return parent::setTimestamp($unixtimestamp);
 	}
 
-	public function setTimezone($timezone)
+	public function setTimezone(\DateTimeZone $timezone) : \DateTime
 	{
 		$this->flag_dirty();
 		return parent::setTimezone($timezone);
@@ -189,13 +189,13 @@ class DateTime extends \DateTime implements DateTimeInterface
 		return parent::modify($modify);
 	}
 	
-	public function add($interval)
+	public function add(\DateInterval $interval) : \DateTime
 	{
 		$this->flag_dirty();
 		return parent::add($interval);
 	}
 
-	public function sub($interval)
+	public function sub(\DateInterval $interval) : \DateTime
 	{
 		$this->flag_dirty();
 		return parent::sub($interval);
